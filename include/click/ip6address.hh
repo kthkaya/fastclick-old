8510839b78642a420daf715251b6b4c6930087aa
tdev <tdev@tdev>
2018-02-12 14:56:18 +0100

    another attepmt at matching

--- include/click/ip6address.hh
+++ include/click/ip6address.hh
@@ -103,7 +103,8 @@ class IP6Address { public:
      * An IPv6 address in the form of "64:ff9b::192.0.2.5" has a WKP
      */
     bool has_wellKnown_prefix() const {
-    	return _addr.s6_addr[0] == 0x64 && data32()[2] == htonl(0xff9b0000);
+    	return _addr.s6_addr[0] == 0x64 && _addr.s6_addr16[1] == 0xff9b;
+    			//data32()[2] == htonl(0xff9b0000);
     }
 
     /** @brief Extract embedded Ethernet address into @a x.
